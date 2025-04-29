0354b1803960e5435482e147b79235b2c9530968
Gabriel Palma Moraes <gpmoraes82@gmail.com>
2025-04-29 17:36:43 -0300

    Ajustes e aprimoramentos de vizualição e padrão de mensagens para usuário

--- Lista_de_exercicios_1-Obrigatorios.js
+++ Lista_de_exercicios_1-Obrigatorios.js
@@ -9,42 +9,48 @@ let num2 = 0;
 
 while (selector !== 0) {
 
+    console.clear(); //limpar o terminal para melhor vizualização 
+
     console.log(`
-        1. Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar utilizando uma estrutura de controle if.
-        2. Crie um programa que classifica a idade de uma pessoa em categorias (criança, adolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de controle if-else.
-        3. Implemente um programa que recebe uma nota de 0 a 10 e classifica como "Aprovado", "Recuperação", ou "Reprovado" utilizando if-else if.
-        4. Crie um menu interativo no console que oferece ao usuário a escolha de três opções. Utilize switch-case para implementar a lógica de cada opção selecionada.
-        5. Escreva um programa que calcula o Índice de Massa Corporal (IMC) de uma pessoa e determina a categoria de peso (baixo peso, peso normal, sobrepeso, obesidade) utilizando if-else.
-        6. Ler três valores para os lados de um triângulo: A, B e C. Verificar se os lados fornecidos formam realmente um triângulo. Caso forme, deve ser indicado o tipo de triângulo: Isósceles, escaleno ou eqüilátero.
-        Para verificar se os lados fornecem um triângulo: A < B + C e B < A + C e C < A + B
-        Triângulo isósceles: possui dois lados iguais (A=B ou A=C ou B=C)
-        Triângulo escaleno: possui todos os lados diferentes (A≠B e B≠C)
-        Triângulo eqüilátero: possui todos os lados iguais (A=B e B=C)
-        7. As maçãs custam R$ 0,30 se forem compradas menos do que uma dúzia, e R$ 0,25 se forem compradas pelo menos doze. Escreva um algoritmo que leia o número de maçãs compradas, calcule e escreva o valor total da compra.
-        8. Escreva um algoritmo para ler 2 valores (considere que não serão lidos valores iguais) e escrevê-los em ordem crescente.
-        9. Implemente um programa que exibe uma contagem regressiva de 10 até 1 no console utilizando um loop for.
-        10. Escreva um algoritmo para ler um número inteiro e escrevê-lo na tela 10 vezes.
-        11. Escreva um programa que solicita ao usuário 5 números e calcula a soma total utilizando um loop for.
-        12. Crie um programa que exibe a tabuada de um número fornecido pelo usuário (de 1 a 10) utilizando um loop for.
-        13. Fazer um algoritmo para receber números decimais até que o usuário digite 0 e fazer a média aritmética desses números.
-        14. Crie um programa que calcula o fatorial de um número fornecido pelo usuário utilizando um loop for ou while.
-        15. Escreva um programa que gera e imprime os primeiros 10 números da sequência de Fibonacci utilizando um loop for.
+    \x1b[33m1.\x1b[0m Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar utilizando uma estrutura de controle if.
+    \x1b[33m2.\x1b[0m Crie um programa que classifica a idade de uma pessoa em categorias (criança, adolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de controle if-else.
+    \x1b[33m3.\x1b[0m Implemente um programa que recebe uma nota de 0 a 10 e classifica como "Aprovado", "Recuperação", ou "Reprovado" utilizando if-else if.
+    \x1b[33m4.\x1b[0m Crie um menu interativo no console que oferece ao usuário a escolha de três opções. Utilize switch-case para implementar a lógica de cada opção selecionada.
+    \x1b[33m5.\x1b[0m Escreva um programa que calcula o Índice de Massa Corporal (IMC) de uma pessoa e determina a categoria de peso (baixo peso, peso normal, sobrepeso, obesidade) utilizando if-else.
+    \x1b[33m6.\x1b[0m Ler três valores para os lados de um triângulo: A, B e C. Verificar se os lados fornecidos formam realmente um triângulo. Caso forme, deve ser indicado o tipo de triângulo: Isósceles, escaleno ou eqüilátero.
+    Para verificar se os lados fornecem um triângulo: A < B + C e B < A + C e C < A + B
+    Triângulo isósceles: possui dois lados iguais (A=B ou A=C ou B=C)
+    Triângulo escaleno: possui todos os lados diferentes (A≠B e B≠C)
+    Triângulo eqüilátero: possui todos os lados iguais (A=B e B=C)
+    \x1b[33m7.\x1b[0m As maçãs custam R$ 0,30 se forem compradas menos do que uma dúzia, e R$ 0,25 se forem compradas pelo menos doze. Escreva um algoritmo que leia o número de maçãs compradas, calcule e escreva o valor total da compra.
+    \x1b[33m8.\x1b[0m Escreva um algoritmo para ler 2 valores (considere que não serão lidos valores iguais) e escrevê-los em ordem crescente.
+    \x1b[33m9.\x1b[0m Implemente um programa que exibe uma contagem regressiva de 10 até 1 no console utilizando um loop for.
+    \x1b[33m10.\x1b[0m Escreva um algoritmo para ler um número inteiro e escrevê-lo na tela 10 vezes.
+    \x1b[33m11.\x1b[0m Escreva um programa que solicita ao usuário 5 números e calcula a soma total utilizando um loop for.
+    \x1b[33m12.\x1b[0m Crie um programa que exibe a tabuada de um número fornecido pelo usuário (de 1 a 10) utilizando um loop for.
+    \x1b[33m13.\x1b[0m Fazer um algoritmo para receber números decimais até que o usuário digite 0 e fazer a média aritmética desses números.
+    \x1b[33m14.\x1b[0m Crie um programa que calcula o fatorial de um número fornecido pelo usuário utilizando um loop for ou while.
+    \x1b[33m15.\x1b[0m Escreva um programa que gera e imprime os primeiros 10 números da sequência de Fibonacci utilizando um loop for.
+    \x1b[31m0.\x1b[0m Sair.
         `);
 
-    selector = Number(prompt("Digite o número do exercício que deseja verificar/válidar: "));
+    selector = Number(prompt(`    Digite o número do exercício que deseja verificar/válidar: `));
+    
+    console.clear();
 
     switch (selector) {
         case 1:
             // Lógica para verificar se o número é par ou ímpar
 
-            num1 = Number(prompt('Verifique se o número é par ou ímpar: '));
-            (num1 % 2 === 0) ? console.log(`${num1} é PAR`) : console.log(`${num1} é IMPAR`);
-            break;
+            num1 = Number(prompt(`    Verifique se o número é par ou ímpar: `));
+            (num1 % 2 === 0) ? console.log(`    ${num1} é PAR`) : console.log(`    ${num1} é IMPAR`);
+            console.log(`\n`);
 
+            break;
         case 2:
             // Classificar idade em categorias
-            
-            num1 = Number(prompt('Digite uma idade: '));
+
+            num1 = Number(prompt(`  Digite uma idade: `));
 
             if (num1 >= 0 && num1 <= 12) {
                 console.log(`${num1} é criança`);
@@ -55,64 +61,116 @@ while (selector !== 0) {
             } else if (num1 > 64) {
                 console.log(`${num1} é idoso`);
             } else {
-                console.log("Idade não existe!");
+                console.log(`   Idade não existe!`);
             }
-            break;
+            console.log(`\n`);
 
+            break;
         case 3:
             // Classificar nota como Aprovado, Recuperação ou Reprovado
-            
-            num1 = Number(prompt('Digite uma nota de 0 a 10: '));
+
+            num1 = Number(prompt(`Digite uma nota de 0 a 10: `));
 
             if (num1 >= 0 && num1 <= 3.9) {
                 console.log(`${num1} Reprovado`);
             } else if (num1 >= 4 && num1 <= 6.9) {
                 console.log(`${num1} Recuperação`);
-            } else if (num1 >= 6.9 && num1 <=10) {
+            } else if (num1 >= 6.9 && num1 <= 10) {
                 console.log(`${num1} Aprovado`);
-            } else console.log("Nota fora do padrão!");
+            } else console.log(`Nota fora do padrão!`);
+            console.log(`\n`);
 
             break;
         case 4:
             // Menu interativo com 3 opções
+
+            console.log(`Escolha qual deseja rolar 2.Uma moeda | 4. Um dado de 4 lados | 6. Um dados de 6 lados:`);
+            num1 = Number(prompt(`Digita o número do item que deseja rolar: `));
+
+            switch (num1) {
+                case 2:
+                    console.log(Math.floor(Math.random() * num1) + 1);
+                    break;
+                case 4:
+                    console.log(Math.floor(Math.random() * num1) + 1);
+                    break;
+                case 6:
+                    console.log(Math.floor(Math.random() * num1) + 1);
+                    break;
+            }
+            console.log(`\n`);
+
             break;
         case 5:
             // Calcular IMC e classificar peso
+            console.log(`\n`);
+
             break;
         case 6:
             // Verificar tipo de triângulo
             break;
         case 7:
             // Calcular valor da compra de maçãs
+            console.log(`\n`);
+
             break;
+
         case 8:
             // Escrever dois valores em ordem crescente
+            console.log(`\n`);
+
             break;
+
         case 9:
             // Contagem regressiva de 10 até 1
+            console.log(`\n`);
+
             break;
+
         case 10:
             // Escrever um número inteiro 10 vezes
+            console.log(`\n`);
+
             break;
+
         case 11:
             // Somar 5 números fornecidos
+            console.log(`\n`);
+
             break;
+
         case 12:
             // Tabuada de um número
+            console.log(`\n`);
+
             break;
+
         case 13:
             // Média de números decimais até digitar 0
+            console.log(`\n`);
+
             break;
+
         case 14:
             // Calcular fatorial
+            console.log(`\n`);
+
             break;
+
         case 15:
             // Gerar sequência de Fibonacci
+            console.log(`\n`);
+
             break;
+
         default:
             // Caso o valor não esteja entre 1 e 15
+            console.log(`\n`);
+
             break;
+
     }
 
-    if (selector !== 0) selector = Number(prompt("\n1-Listar exercícicos novamente ou 0-Sair: "));
+    if (selector !== 0) selector = Number(prompt(`\x1b[33m1.\x1b[0m Listar exercícicos novamente ou \x1b[31m0.\x1b[0m Sair: `));
+
 }
